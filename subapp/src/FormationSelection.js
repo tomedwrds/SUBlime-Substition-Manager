511a0122326c66f9b53eb93c091a92fa6379a003
tomedwrds <60488797+tomedwrds@users.noreply.github.com>
2022-06-21 19:48:42 +1200

    tidied up code and commented

--- subapp/src/FormationSelection.js
+++ subapp/src/FormationSelection.js
@@ -6,21 +6,27 @@ import GameField from "./in_game/GameField";
 
 
 const FormationSelection = () =>
-{   const positionSelectionData =   [
+{   
+
+    //This is the data sets used for the display of the views 
+    const positionSelectionData =   [
     
     {   layoutId: 0,
         layoutName: 'The Classic', 
-        layoutData: [[0,0,0,'CF',0,0,0],
+        layoutData: [
+                [0,0,0,'CF',0,0,0],
                 ['LF',0,0,0,0,0,'RF'],
                 [0,'LI',0,0,0,'RI',0],
                 [0,0,0,'CH',0,0,0],
                 ['LH',0,0,0,0,0,'RH'],
                 [0,0,'CB',0,'CB',0,0],
-                [0,0,0,'GK',0,0,0]]},
+                [0,0,0,'GK',0,0,0]]
+    },
     {
         layoutId:1,
         layoutName: '3-4-3', 
-        layoutData: [[0,0,0,'CF',0,0,0],
+        layoutData: [
+                [0,0,0,'CF',0,0,0],
                 [0,'LF',0,0,0,'RF',0],
                 [0,0,0,0,0,0,0],
                 ['LH',0,'CH',0,'CH',0,'RH'],
@@ -31,7 +37,8 @@ const FormationSelection = () =>
     {
         layoutId:2,
         layoutName: 'Park the Bus', 
-        layoutData: [[0,0,0,'CF',0,0,0],
+        layoutData: [
+                [0,0,0,'CF',0,0,0],
                 [0,0,0,0,0,0,0],
                 [0,'RI',0,0,0,'LI',0],
                 [0,0,'CH',0,'CH',0,0],
@@ -42,58 +49,52 @@ const FormationSelection = () =>
     {
         layoutId:3,
         layoutName: '4-4-2', 
-        layoutData: [[0,0,0,0,0,0,0],
-        [0,'LF',0,0,0,'RF',0],
-        [0,0,0,0,0,0,0],
-        ['LH',0,'CH',0,'CH',0,'RH'],
-        [0,0,0,0,0,0,0],
-        ['LB',0,'CB',0,'CB',0,'RB'],
-        
-        [0,0,0,'GK',0,0,0]]
+        layoutData: [
+                [0,0,0,0,0,0,0],
+                [0,'LF',0,0,0,'RF',0],
+                [0,0,0,0,0,0,0],
+                ['LH',0,'CH',0,'CH',0,'RH'],
+                [0,0,0,0,0,0,0],
+                ['LB',0,'CB',0,'CB',0,'RB'],
+                [0,0,0,'GK',0,0,0]]
     },
     {
         layoutId:4,
         layoutName: '2-2-2', 
-        layoutData: [[0,0,0,0,0,0,0],
-        [0,0,'LF',0,'RF',0,0],
-        [0,0,0,0,0,0,0],
-        [0,0,'LH',0,'RH',0,0],
-        [0,0,0,0,0,0,0],
-        [0,0,'LB',0,'RB',0,0],
-        
-        [0,0,0,'GK',0,0,0]]
+        layoutData: [
+                [0,0,0,0,0,0,0],
+                [0,0,'LF',0,'RF',0,0],
+                [0,0,0,0,0,0,0],
+                [0,0,'LH',0,'RH',0,0],
+                [0,0,0,0,0,0,0],
+                [0,0,'LB',0,'RB',0,0],
+                [0,0,0,'GK',0,0,0]]
     },
     {
         layoutId:5,
         layoutName: '3-3', 
-        layoutData: [[0,0,0,,0,0,0],
-        [0,'LF',0,'CF',0,'RF',0],
-        [0,0,0,0,0,0,0],
-        
-        [0,0,0,0,0,0,0],
-        [0,'LB',0,'CB',0,'RB',0],
-        [0,0,0,0,0,0,0],
-        
-        
-        [0,0,0,'GK',0,0,0]]
+        layoutData: [
+                [0,0,0,,0,0,0],
+                [0,'LF',0,'CF',0,'RF',0],
+                [0,0,0,0,0,0,0],
+                [0,0,0,0,0,0,0],
+                [0,'LB',0,'CB',0,'RB',0],
+                [0,0,0,0,0,0,0],
+                [0,0,0,'GK',0,0,0]]
     },
 
-
-
-
-
-     ]
-
-     const[selectedLayout,setSelectedLayout] = useState(null)
-     
-     console.log(selectedLayout)
+]
+    //Hook used to store what the current selected layout is
+    const[selectedLayout,setSelectedLayout] = useState(null)
+   
+    //Flat list is used that renders a field of the data
     return(
         <FlatList
         renderItem={(item) => GameField(item,setSelectedLayout,selectedLayout)}
         data = {positionSelectionData}
         numColumns = {2}
         keyExtractor = {item => item.layoutId}
-        ></FlatList>
+        />
         
     )
 }
--- subapp/src/in_game/GameField.js
+++ subapp/src/in_game/GameField.js
@@ -1,87 +1,93 @@
 import React from "react"
-import { View,FlatList,StyleSheet,Text, Pressable } from "react-native"
-import { useState } from "react"
+import { View,StyleSheet,Text, Pressable } from "react-native"
 
-import PlayerIcon from './PlayerIcon.js'
 
 
 const GameField = ({item},setSelectedLayout,selectedLayout) => {
 
-
-    
-  
+    //Cutsomized styling to show what component is currently selected
     let selectedColor = '#D3D3D3'
-    if (item.layoutId ==selectedLayout) {selectedColor = '#6ba1c4'}
+    if (item.layoutId ==selectedLayout)
+    {
+        selectedColor = '#6ba1c4'
+    }
  
     return(
-        <View style = {{flex:1}}>
-        <Pressable onPress = {()=>setSelectedLayout(item.layoutId)}>
-        <View style = {{...styles.gamePitchContainer,backgroundColor:selectedColor}}>
-        <View style = {{flex: 1}}>
-            <Text style = {styles.nameText}>{item.layoutName}</Text>
-            
-        </View>
-        <View style ={styles.gamePitch}  >
-        
-        <View style = {{position: 'absolute',height: '100%',width:'100%',borderWidth:2,borderColor:'white',borderRadius:20,overflow:'hidden'}}>
-          <View style = {{flex: 1,flexDirection:'row', backgroundColor: 'green',borderBottomWidth:2,borderColor:'white'}}>
-            <View style = {{flex:1, backgroundColor:'green'}}></View>
-            <View style = {{flex:4}}>
-                <View style = {{flex:17,borderRightWidth:2,borderLeftWidth:2,borderBottomWidth:2,borderColor:'white', borderBottomEndRadius:150,borderBottomStartRadius:150}}></View>
-                <View style ={{flex:3}}></View>
-            </View>
-            
-            <View style = {{flex:1, backgroundColor:'green'}}></View>
-          </View>
-          <View style = {{flex: 1, backgroundColor: 'green',borderBottomWidth:2,borderColor:'white'}}></View>
-          <View style = {{flex: 1, backgroundColor: 'green',borderBottomWidth:2,borderColor:'white'}}></View>
-          <View style = {{flex: 1, backgroundColor: 'green',flexDirection:'row'}}>
-            <View style = {{flex:1, backgroundColor:'green'}}></View>
-            <View style = {{flex:4}}>
-                <View style = {{flex:3}}></View>
-                <View style = {{flex:17,borderRightWidth:2,borderLeftWidth:2,borderTopWidth:2,borderColor:'white', borderTopLeftRadius:150,borderTopRightRadius:150}}></View>
-            </View>
-            <View style = {{flex:1, backgroundColor:'green'}}></View>
-          </View>
-        </View>    
-          
-                             
-                {item.layoutData.map((prop,index) =>
-                {
-                    return(
-                        <View key = {index} style = {{flex: 1,flexDirection:'row'}}>
-                            {prop.map((prop,index) => {
-                                let opacity_ = 1
-                                if (prop == 0)  opacity_ = 0
-                                return(
-                                    
-                                    <View key = {index} style ={{...styles.iconBodyStyle, opacity: opacity_}}>
-                                        <Text style = {styles.iconText}>{prop != 0 ? prop:''}</Text>
-                                    </View>
-                                    
-                                    )
-                            
-                            })}
-                        </View>
-                    )
-                })}
-             
-               
-                    
+        //Pressable wrap is used for determing when elements have been pressed
+        <Pressable style = {{flex:1}} onPress = {()=>setSelectedLayout(item.layoutId)}>
+
+            {/* gamePitchContainer is the wrapping view of all elemenets all formating of background color, border, margin etc is in this */}
+            <View style = {{...styles.gamePitchContainer,backgroundColor:selectedColor}}>
                 
-               
+                {/* Tile text */}
+                <Text style = {styles.nameText}>{item.layoutName}</Text>
+                {/* View wrapper needs to exist to allow magic fuckery of absolute positioning  */}
+                <View >
+                    {/* Game pitch serves as view for all absolute drawing to take place within */}
+                    <View style = {styles.gamePitch}>
 
+                        {/* Top quarter */}
+                        <View style = {{flex: 1,flexDirection:'row', backgroundColor: 'green',borderBottomWidth:2,borderColor:'white'}}>
+                            <View style = {{flex:1, backgroundColor:'green'}}></View>
+                            <View style = {{flex:4}}>
+                                <View style = {{flex:17,borderRightWidth:2,borderLeftWidth:2,borderBottomWidth:2,borderColor:'white', borderBottomEndRadius:150,borderBottomStartRadius:150}}></View>
+                                <View style ={{flex:3}}></View>
+                            </View>
+                            <View style = {{flex:1, backgroundColor:'green'}}></View>
+                        </View>
+                        {/* Middle half of field */}
+                        <View style = {{flex: 1, backgroundColor: 'green',borderBottomWidth:2,borderColor:'white'}}></View>
+                        <View style = {{flex: 1, backgroundColor: 'green',borderBottomWidth:2,borderColor:'white'}}></View>
+                        
+                        {/* Bottom quarter */}
+                        <View style = {{flex: 1, backgroundColor: 'green',flexDirection:'row'}}>
+                            <View style = {{flex:1, backgroundColor:'green'}}></View>
+                            <View style = {{flex:4}}>
+                                <View style = {{flex:3}}></View>
+                                <View style = {{flex:17,borderRightWidth:2,borderLeftWidth:2,borderTopWidth:2,borderColor:'white', borderTopLeftRadius:150,borderTopRightRadius:150}}></View>
+                            </View>
+                            <View style = {{flex:1, backgroundColor:'green'}}></View>
+                        </View>
+                    </View>    
+                    
+                    {/* Mapping of all icons to the pitch.
+                     the first map creates the rows than the second map renders the icons   */}
+                    {item.layoutData.map((prop,index) =>
+                    {
+                        return(
+                            <View key = {index} style = {{flex: 1,flexDirection:'row'}}>
+                                
+                                {/* Mapping the icons */}
+                                {prop.map((prop,index) => {
+                                    
+                                    //All icons need to be rendred due to how the positioning of them is depedente on flex
+                                    //However we dont want all icons to appear so a transparency is applied if they shouldnt appear
+
+                                    let opacity_ = 1
+                                    if (prop == 0)  opacity_ = 0
+                                    
+                                    return(
+                                        
+                                        <View key = {index} style ={{...styles.iconBodyStyle, opacity: opacity_}}>
+                                            <Text style = {styles.iconText}>{prop != 0 ? prop:''}</Text>
+                                        </View>
+                                        
+                                    )
+                                
+                                })}
+                            </View>
+                        )
+                    })}
 
                 </View>
-                </View>
-                </Pressable></View>
+            </View>
+        </Pressable>
     )
 }
 
 const styles = StyleSheet.create({
     gamePitchContainer: {
         flex:1,
-        
         paddingBottom:20,
         paddingLeft:20,
         paddingRight:20,
@@ -91,11 +97,15 @@ const styles = StyleSheet.create({
         marginVertical: 20
     },
     gamePitch: {
-        
-        flex: 9,
-        
-        
+        position: 'absolute',
+        height: '100%',
+        width:'100%',
+        borderWidth:2,
+        borderColor:'white',
+        borderRadius:20,
+        overflow:'hidden'
     },
+  
     nameText: {
         fontSize:20,
         textAlign:'center',
